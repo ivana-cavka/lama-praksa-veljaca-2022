c6557c374dc09bafc5cffacbb891fdf4039407f6
MajaParadzik <92535429+MajaParadzik@users.noreply.github.com>
2022-03-15 14:46:29 +0100

    add new

--- project/client/zadatak/src/app/views/zadatak/zadatak-view/zadatak-view.component.ts
+++ project/client/zadatak/src/app/views/zadatak/zadatak-view/zadatak-view.component.ts
@@ -27,7 +27,7 @@ export class ZadatakViewComponent implements OnInit {
   onSave() {
     console.log(this.form.value);
     this.ZadatakService.add(this.form.value).subscribe((result) => {
-      this.router.navigateByUrl('/');
+      this.router.navigateByUrl('/atributi-zadataka');
     });
   }
 }
