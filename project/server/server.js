4fdaa9491773cc0c1e848ad2ca1b4fa3057da05d
MajaParadzik <92535429+MajaParadzik@users.noreply.github.com>
2022-03-16 09:47:06 +0100

    server add

--- project/server/server.js
+++ project/server/server.js
@@ -1,6 +1,6 @@
-const express=require("express");
-const app=express();
-const port=3000;
+const express = require("express");
+const app = express();
+const port = 3000;
 const fs = require("fs");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -23,44 +23,63 @@ app.use(cors());
 
 const getData = (path) => JSON.parse(fs.readFileSync(path));
 
-app.get("/atributi-zadataka",(req,res)=>{
-    const zadatci = getData("db/zadatci.json");
-    /* res.send( {zadatci} );  */
-    res.send( zadatci); 
-    /* console.log("Hello from server. "); */
+app.get("/atributi-zadataka", (req, res) => {
+  const zadatci = getData("db/zadatci.json");
+  /* res.send( {zadatci} );  */
+  res.send(zadatci);
+  /* console.log("Hello from server. "); */
 });
 
-app.get("/atributi-zadataka/one",(req, res) => {
-    const { id } = req.query;
+app.get("/atributi-zadataka/one", (req, res) => {
+  const { id } = req.query;
 
-    const zadatci = getData("db/zadatci.json");
+  const zadatci = getData("db/zadatci.json");
 
-    const assignment = zadatci.find( (zadatak) => zadatak.id === +id );   
-    res.send({ zadatak: assignment });
+  const assignment = zadatci.find((zadatak) => zadatak.id === +id);
+  res.send({ zadatak: assignment });
 });
 
-app.get("/atributi-predmeta",(req,res)=>{
-    const zadatci = getData("db/predmeti.json");
-    /* res.send( {zadatci} );  */
-    res.send( zadatci); 
-    /* console.log("Hello from server. "); */
+app.get("/atributi-predmeta", (req, res) => {
+  const predmeti = getData("db/predmeti.json");
+  /* res.send( {predmeti} );  */
+  res.send(predmeti);
+  /* console.log("Hello from server. "); */
+});
+
+app.get("/atributi-predmeta/one", (req, res) => {
+  const { id } = req.query;
+
+  const predmeti = getData("db/predmeti.json");
+
+  const assignment = predmeti.find((predmet) => predmet.id === +id);
+  res.send({ predmet: assignment });
 });
 
 /* app.get("/", (req, res) => { */
-    app.get("/atributi-pismena", (req, res) => {
-    const arr = JSON.parse(fs.readFileSync("database/atributi-pismena.json"));
-    res.send({arr});
-  });
-app.listen(port,()=>{
-    console.log("> Server is up and running on port: "+port)
+app.get("/atributi-pismena", (req, res) => {
+  const arr = JSON.parse(fs.readFileSync("database/atributi-pismena.json"));
+  res.send({ arr });
+});
+app.listen(port, () => {
+  console.log("> Server is up and running on port: " + port);
 });
 
 app.post("/atributi-zadataka", (req, res) => {
-    let atributi = readDb("db/zadatci.json");
-  
-    atributi.push({
-      ...req.body
-    });
-    saveDb("db/zadatci.json", atributi);
-    res.send({ atributi });
+  let atributi = readDb("db/zadatci.json");
+
+  atributi.push({
+    ...req.body,
   });
+  saveDb("db/zadatci.json", atributi);
+  res.send({ atributi });
+});
+
+app.post("/atributi-predmeti", (req, res) => {
+  let atributi = readDb("db/predmeti.json");
+
+  atributi.push({
+    ...req.body,
+  });
+  saveDb("db/predmeti.json", atributi);
+  res.send({ atributi });
+});
